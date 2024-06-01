deb5789aec8281624315793a176cb9aa388bdec8
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2024-06-01 23:26:59 +0300

    chore: Add Flask configuration and login form

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 # Description: This file is the initializer of the app package. It imports the routes module that defines the URL routes for the application, and then it creates an application instance.
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
